6d5c2635adbabe72ee7417d2aef55a99fb1dde6a
Saksham <gupta.10@iitj.ac.in>
2019-06-11 16:08:50 +0530

    display headers

--- src/components/DisplayData.vue
+++ src/components/DisplayData.vue
@@ -1,7 +1,22 @@
 <template>
   <v-container>
     <v-layout text-xs-center wrap>
-      <v-card-text>Hello World</v-card-text>
+      <v-card-text>
+        <v-select
+          v-model="header_num"
+          :items="template.headers"
+          required
+          label="Choose Header"
+          outline
+        ></v-select>
+        <v-select
+          v-model="clause_num"
+          :items="template.clauses"
+          required
+          label="Choose Clauses"
+          outline
+        ></v-select>
+      </v-card-text>
     </v-layout>
   </v-container>
 </template>
@@ -11,27 +26,36 @@ export default {
   name: "DisplayData",
   data() {
     return {
-      baseURL: "http://localhost:3000/"
+      baseURL: "http://localhost:3000/",
+      template: { header: '', clauses: [] },
+      header_num: "",
+      clause_num: ""
     };
   },
   methods: {
     headerData() {
+      let self = this;
       axios.get(this.baseURL + "headerdata/headerdata").then(response => {
         console.log("headers", response.data);
+        response.data.forEach(function(element) {
+            console.log(self.template.length)
+            self.template.push(response.data.length)
+            console.log(self.template.length)
+          self.template.header = element.Description;
+        });
       });
     },
     clauseData() {
       axios.get(this.baseURL + "headerdata/clausedata").then(response => {
         console.log("clauses", response.data);
         response.data.forEach(function(element) {
-          var array = element.ItemId.split('-');
-          console.log(array)
+          var array = element.ItemId.split("-");
+          console.log(array);
         });
-        // var array = response.data.ItemId.toString().split("-");
-        // console.log(array);
       });
     }
   },
+  computed: {},
 
   mounted() {
     // console.log("hello");
